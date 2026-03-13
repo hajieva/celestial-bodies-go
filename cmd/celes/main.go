9777fe34c8e2b675e2d653b4fcc01b8358d6912f
hajieva <cmaralhc17@gmail.com>
2026-03-13 15:31:03 +0300

    api-endpoints created

--- cmd/celes/main.go
+++ cmd/celes/main.go
@@ -5,21 +5,26 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strings"
+
 	"github.com/hajieva/celestial-bodies-go/internal/data"
 )
-func loadMoons() []data.Moon{
+
+var planets []data.Planet
+var moons []data.Moon
+
+func loadMoons() []data.Moon {
 	file, err := os.Open("moons.json")
-	if err != nil{
+	if err != nil {
 		fmt.Println("Error opening moons.json:", err)
 		return nil
 	}
 
 	defer file.Close()
-
-	var moons [] data.Moon
+	var moons []data.Moon
 	decoder := json.NewDecoder(file)
 	err = decoder.Decode(&moons)
-	if err != nil{
+	if err != nil {
 		fmt.Println("Error decoding moons.json:", err)
 		return nil
 	}
@@ -29,16 +34,16 @@ func loadMoons() []data.Moon{
 }
 func loadPlanets() []data.Planet {
 	file, err := os.Open("planets.json")
-	if err != nil{
+	if err != nil {
 		fmt.Println("Error opening planets.json", err)
 		return nil
 	}
 	defer file.Close()
-	
+
 	var planets []data.Planet
 	decoder := json.NewDecoder(file)
-	err = decoder.Decode(&planets)
-	if err != nil{
+	err = decoder.Decode(&planets) // why store in memory?
+	if err != nil {
 		fmt.Println("Error decoding planets.json", err)
 		return nil
 	}
@@ -48,25 +53,82 @@ func loadPlanets() []data.Planet {
 
 func main() {
 	fmt.Println("Celestial API")
-	
-	moons := loadMoons()
-	planets := loadPlanets()
+	planets = loadPlanets()
+	moons = loadMoons()
+
 	fmt.Printf("Loaded %d planets and %d moons\n", len(planets), len(moons))
 
-	
-	//http.HandleFunc("/planets", get Planets)
+	http.HandleFunc("/bodies/planets", getPlanets)
+	http.HandleFunc("/bodies/planets/{name}", getPlanet)
+	http.HandleFunc("/bodies/planets/{name}/moons", getPlanetMoons)
+	http.HandleFunc("/bodies/moons/{name}", getMoon)
 
-	//http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", nil)
 }
 
-
-
 func getPlanets(w http.ResponseWriter, r *http.Request) {
-	//mars := Planets{Name: "Mars",
-	//Mass:       0.64171,
-	//Satellites: []string{"Phobos", "Deimos"}}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(planets)
+}
+func getPlanet(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 
-	//w.Header().Set("Content-Type", "application/json")
-	//w.WriteHeader(http.StatusOK)
-	//json.NewEncoder(w).Encode(planets)
+	// how to get planet name from URL?
+	planetName := r.PathValue("name")
+
+	for _, planet := range planets {
+		if strings.EqualFold(planet.Name, planetName) {
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(planet)
+			return
+		}
+	}
+	http.Error(w, "Planet not found", http.StatusNotFound)
+
+}
+
+var moonCount int
+var counter int
+
+func getPlanetMoons(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	// specific planet's moon details
+	// find the planet first
+	// find that planet's moons
+	planetName := r.PathValue("name")
+
+	for _, planet := range planets {
+
+		if strings.EqualFold(planetName, planet.Name) {
+
+			moonCount = planet.MoonsCount
+
+			for _, moons := range moons {
+				if strings.EqualFold(moons.ParentPlanet, planet.Name) {
+					w.WriteHeader(http.StatusOK)
+					json.NewEncoder(w).Encode(moons)
+					if counter == moonCount {
+						return
+					}
+					counter++
+				}
+			}
+		}
+	}
+	http.Error(w, "Planet or moons not found", http.StatusNotFound)
+
+}
+func getMoon(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	moonName := r.PathValue("name")
+	for _, moon := range moons {
+		if strings.EqualFold(moon.Name, moonName) {
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(moon)
+			return
+		}
+	}
+	http.Error(w, "Moon not found", http.StatusNotFound)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/hajieva/celestial-bodies-go
 
-go 1.18
+go 1.26
